a8a9e663767978a9f34d738b968c5b94e48de4bc
Lennard <admin@snodecoder.doorhetgeluid.nl>
2025-10-09 17:03:57 +0200

    fix

--- src/IntuneCD/intunecdlib/documentation_functions.py
+++ src/IntuneCD/intunecdlib/documentation_functions.py
@@ -267,7 +267,7 @@ def clean_list(data, decode):
         else:
             values.append(item)
 
-    return values
+    return escape_markdown(values)
 
 
 def write_type_header(split, outpath, header):
