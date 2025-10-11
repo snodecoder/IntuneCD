b19431dbc72d6cca04ea1d2c5ee57f3cc20bb099
Lennard <admin@snodecoder.doorhetgeluid.nl>
2025-10-11 21:09:53 +0200

    fux

--- src/IntuneCD/intunecdlib/documentation_functions.py
+++ src/IntuneCD/intunecdlib/documentation_functions.py
@@ -673,7 +673,7 @@ def extract_setting(setting_instance, settings_lookup):
     description = convert_newlines_to_br(description)
     # Append info URLs to description
     if info_urls:
-        links = " ".join([f'[More info {i+1}]({url})' for i, url in enumerate(info_urls)])
+        links = " ".join([f'{url}<br>)' for url in enumerate(info_urls)])
         description = f"{description}<br>{links}" if description else links
     description = f"<details><summary>Click to expand...</summary>{description}</details>" if description else ""
 
